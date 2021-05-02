36f83361b3c1b40b1d949972935997bc37a1ac5f
sheila <devsheilawambui@gmail.com>
2021-05-02 21:22:58 +0300

    add Animal getters and equals and hash code methods

--- src/main/java/Animal.java
+++ src/main/java/Animal.java
@@ -4,7 +4,7 @@ import org.sql2o.Sql2oException;
 import java.util.List;
 import java.util.Objects;
 
-public class Animal {
+public class Animal implements DbManagement {
     public int id;
     public String name;
 
@@ -17,6 +17,49 @@ public class Animal {
         this.name = name;
         this.type = type;
 
+
+    }
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Animal)) return false;
+        Animal animal = (Animal) o;
+        return Objects.equals(getName(), animal.getName()) && Objects.equals(getType(), animal.getType());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getType());
+    }
+
+
+    @Override
+    public void save() {
+
+    }
+
+    @Override
+    public void update() {
 
     }
 }
