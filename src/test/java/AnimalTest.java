d883c034557567972d17c6f8bf810f64a84833a0
sheila <devsheilawambui@gmail.com>
2021-05-02 21:32:27 +0300

    test animal getters

--- src/test/java/AnimalTest.java
+++ src/test/java/AnimalTest.java
@@ -8,12 +8,26 @@ import static org.junit.Assert.*;
 import java.util.List;
 
 public class AnimalTest {
-
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
 
     @Test
     public void animal_instantiatesCorrectly_true() {
         Animal testAnimal = new Animal("Hare", "common");
 
         assertEquals(true, testAnimal instanceof Animal);
+    }
+
+    @Test
+    public void getName_AnimalInstantiatesWithName_Hare() {
+        Animal testAnimal = new Animal("Hare", "common");
+
+        assertEquals("Hare", testAnimal.getName());
+    }
+
+    @Test
+    public void getType_AnimalInstantiatesWithType_Common() {
+        Animal testAnimal = new Animal("Hare", "common");
+        assertEquals("common", testAnimal.getType());
     }
 }
