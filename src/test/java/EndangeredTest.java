e43c23b67a83b00dec909828ef737be214255dad
sheila <devsheilawambui@gmail.com>
2021-05-02 21:51:33 +0300

    test for correct instantiation of endangerd animal

--- src/test/java/EndangeredTest.java
+++ src/test/java/EndangeredTest.java
@@ -1,2 +1,20 @@
+import org.junit.*;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+
+import java.util.List;
+
 public class EndangeredTest {
+    @Rule
+    public DatabaseRule database = new DatabaseRule();
+
+    @Test
+    public void animal_instantiatesCorrectly_true() {
+        EndangeredAnimal testAnimal = new EndangeredAnimal("Bonobo", "endangered", "ill", "adult");
+
+        assertEquals(true, testAnimal instanceof Animal);
+    }
 }
